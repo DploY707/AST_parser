162cec3af627a12577dde0a81cc1938515c759e5
DploY <starj1024@gmail.com>
2021-07-08 21:54:53 +0900

    Implement ExpressionStatement, LocalDeclarationSatement, ReturnStatement, ThrowStatement classes and parsing modules to parse them

--- ASTParser/core/parser.py
+++ ASTParser/core/parser.py
@@ -1,7 +1,291 @@
-class ASTParser(ast):
+from pprint import pprint
+from copy import deepcopy as copy_instance
+
+from core.statements import Statement
+from core.statements import BlockStatement
+from core.statements import ExpressionStatement
+from core.statements import LocalDeclarationStatement
+from core.statements import ReturnStatement
+from core.statements import ThrowStatement
+from core.statements import JumpStatement
+from core.statements import DoStatement
+from core.statements import WhileStatement
+from core.statements import TryStatement
+from core.statements import SwitchStatement
+from core.statements import BlockStatement
+
+from core.utils import Color
+from core.utils import set_string_colored
+
+stmtList = [
+        'ExpressionStatement',
+        'LocalDeclarationStatement',
+        'ReturnStatement',
+        'ThrowStatement',
+        'JumpStatement',
+        'DoStatement',
+        'WhileStatement',
+        'TryStatement',
+        'IfStatement',
+        'SwitchStatement',
+        'BlockStatement',
+        ]
+
+actionList = [
+        'ArrayAccess',
+        'ArrayCreation',
+        'ArrayInitializer',
+        'Assignment',
+        'BinaryInfix',
+        'Cast',
+        'FieldAccess',
+        'Literal',
+        'Local',
+        'MethodInvocation',
+        'Parenthesis',
+        'TypeName',
+        'Unary',
+        'Dummy',
+        ]
+
+
+class ASTParser():
     def __init__(self):
         self.ast = None
         self.errMsg = ''
+        self.parsedNodes = list()
+        self.parsedEdges = list()
 
-    def load_ast(ast):
-        self.ast = ast
+    def print_parsing_error(self, errType):
+        if errType == 1:
+            self.errMsg = 'AST is not loaded, so you have to load it first'
+        elif errorType == 2:
+            self.errMsg = 'Wrong formmated AST is entered'
+        else :
+            self.errMsg = 'TODO'
+
+        print(set_string_colored(self.errMsg, Color.RED.value))
+
+    def load_ast(self, ast):
+        self.ast = ast['body']
+
+    def parse_ast(self):
+        print(set_string_colored('DP Check : function: parse_ast is invoked', Color.GREEN.value))
+
+        # self.initialize_ast()
+        self.visit_tree(self.ast)
+
+        print(len(self.parsedNodes))
+        
+    def visit_tree(self, ast):
+        print(set_string_colored('DP Check : function: visit_tree is invoked', Color.GREEN.value))
+        print(set_string_colored("DP Check : " + str(ast), Color.GREEN.value))
+
+        pprint(ast)
+
+        if ast[0] in stmtList:
+            self.visit_statments(ast)
+        elif ast[0] in actionList:
+            self.visit_actions(ast)
+        else:
+            pass
+        '''
+        if type(ast) == type(list()):
+            for subTree in ast:
+                if subTree[0] in stmtList:
+                    self.visit_statments(subTree)
+                elif subTree[0] in actionList:
+                    self.visit_actions(subTree)
+                else:
+                    pass
+        else:
+            if ast in stmtList:
+                self.visit_statments(subTree)
+            elif ast in actionList:
+                self.visit_actions(subTree)
+            else:
+                pass
+        '''
+
+    def visit_statments(self, astBlock):
+        print(set_string_colored('DP Check : function: visit_statments is invoked', Color.GREEN.value))
+        pprint(astBlock)
+
+        if astBlock[0] == 'BlockStatement':
+            stmt = copy_instance(astBlock)
+            stmt[2] = 'extended'
+
+            bs = BlockStatement(stmt)
+            stmtNode = ASTNode(bs, len(self.parsedNodes))
+
+            self.parsedNodes.append(stmtNode)
+
+            for subTree in astBlock[2]:
+                self.visit_tree(subTree)
+
+        elif astBlock[0] == 'ExpressionStatement':
+            stmt = copy_instance(astBlock)
+
+            if type(astBlock[1]) == type(list()):
+                stmt[1] = 'extended'
+
+                es = ExpressionStatement(stmt)
+                stmtNode = ASTNode(es, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+                if type(astBlock[1][0]) == type(list()):
+                    for subTree in astBlock[1]:
+                        self.visit_tree(subTree)
+                else :
+                    self.visit_tree(astBlock[1])
+            else:
+                es = ExpressionStatement(stmt)
+                stmtNode = ASTNode(es, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+        elif astBlock[0] == 'LocalDeclarationStatement':
+            stmt = copy_instance(astBlock)
+
+            if type(astBlock[1]) == type(list()):
+                stmt[1] = 'extended'
+
+                lds = LocalDeclarationStatement(stmt)
+                stmtNode = ASTNode(lds, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+                if type(astBlock[1][0]) == type(list()):
+                    for subTree in astBlock[1]:
+                        self.visit_tree(subTree)
+                else :
+                    self.visit_tree(astBlock[1])
+            else:
+                lds = LocalDeclarationStatement(stmt)
+                stmtNode = ASTNode(lds, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+        elif astBlock[0] == 'ReturnStatement':
+            stmt = copy_instance(astBlock)
+
+            if type(astBlock[1]) == type(list()):
+                stmt[1] = 'extended'
+
+                rs = ReturnStatement(stmt)
+                stmtNode = ASTNode(rs, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+                if type(astBlock[1][0]) == type(list()):
+                    for subTree in astBlock[1]:
+                        self.visit_tree(subTree)
+                else :
+                    self.visit_tree(astBlock[1])
+            else:
+                rs = ReturnStatement(stmt)
+                stmtNode = ASTNode(rs, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+        elif astBlock[0] == 'ThrowStatement':
+            stmt = copy_instance(astBlock)
+
+            if type(astBlock[1]) == type(list()):
+                stmt[1] = 'extended'
+
+                ts = ThrowStatement(stmt)
+                stmtNode = ASTNode(ts, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+                if type(astBlock[1][0]) == type(list()):
+                    for subTree in astBlock[1]:
+                        self.visit_tree(subTree)
+                else :
+                    self.visit_tree(astBlock[1])
+            else:
+                ts = ThrowStatement(stmt)
+                stmtNode = ASTNode(ts, len(self.parsedNodes))
+
+                self.parsedNodes.append(stmtNode)
+
+        elif astBlock[0] == 'JumpStatement':
+            pass
+
+        elif astBlock[0] == 'DoStatement':
+            pass
+
+        elif astBlock[0] == 'WhileStatement':
+            pass
+
+        elif astBlock[0] == 'TryStatement':
+            pass
+
+        elif astBlock[0] == 'IfStatement':
+            stmt = copy_instance(astBlock)
+
+        elif astBlock[0] == 'SwitchStatement':
+            pass
+
+    def visit_actions(self, astBlock):
+        print(set_string_colored('DP Check : function: visit_actions is invoked', Color.GREEN.value))
+        pprint(astBlock)
+
+        if astBlock[0] == 'ArrayAccess':
+            pass
+
+        elif astBlock[0] == 'ArrayCreation':
+            pass
+
+        elif astBlock[0] == 'ArrayInitializer':
+            pass
+
+        elif astBlock[0] == 'Assignment':
+            pass
+
+        elif astBlock[0] == 'BinaryInfix':
+            pass
+
+        elif astBlock[0] == 'Cast':
+            pass
+
+        elif astBlock[0] == 'FieldAccess':
+            pass
+
+        elif astBlock[0] == 'Literal':
+            pass
+
+        elif astBlock[0] == 'Local':
+            pass
+            
+        elif astBlock[0] == 'MethodInvocation':
+            pass
+
+        elif astBlock[0] == 'Parenthesis':
+            pass
+
+        elif astBlock[0] == 'TypeName':
+            pass
+
+        elif astBlock[0] == 'Unary':
+            pass
+
+        else:
+            pass
+
+
+    def show_whole_ast(self):
+        if self.ast:
+            pprint(self.ast)
+        else:
+            self.print_parsing_error(1)
+
+
+# Class for parsed node of AST
+class ASTNode():
+    def __init__(self, stmt, index):
+        self.stmt = stmt
+        self.index = index
+        
--- ASTParser/parser_main.py
+++ ASTParser/parser_main.py
@@ -7,49 +7,13 @@ import copy
 from androguard.decompiler.dad.decompile import DvMethod
 from androguard.misc import AnalyzeAPK
 
+from core.parser import ASTParser
+from core.statements import Statement
+
 import networkx as nx
 
 targetPath = 'data/okhttp-3.1.0_dex.jar'
 resultPath = './out.pickle'
-
-BLOCKSTAT = 'BlockStatement'
-
-stmtList_no_expr = [
-        'JumpStatement',
-        ]
-
-stmtList_expr_in_1st = [
-        'ExpressionStatement',
-        'LocalDeclarationStatement',
-        'ReturnStatement',
-        'ThrowStatement',
-        ]
-
-#Condition Stat
-stmtList_expr_in_2nd = [
-        'DoStatement',
-        'WhileStatement',
-        'TryStatement',
-        'IfStatement',
-        'SwitchStatement',
-        ]
-
-actionList = [
-        'ArrayAccess',
-        'ArrayCreation',
-        'ArrayInitializer',
-        'Assignment',
-        'BinaryInfix',
-        'Cast',
-        'FieldAccess',
-        'Literal',
-        'Local',
-        'MethodInvocation',
-        'Parenthesis',
-        'TypeName',
-        'Unary',
-        'Dummy',
-        ]
 
 def create_ast(method):
     if method.is_external():
@@ -63,60 +27,15 @@ def create_ast(method):
     except AttributeError as ae:
         print('ERROR : in creat_ast()')
 
-def parse_ast(ast) :
-    contents = ast['body']
-    parsed_tree = bfs_for_ast(contents)
-
-def bfs_for_ast(contents):
-    if contents[0] == BLOCKSTAT:
-        bfs_for_blockStat_ast(contents[2])
-    else:
-        bfs_for_normalStat_ast(contents)
-
-def bfs_for_blockStat_ast(contents):
-    for content in contents:
-        bfs_for_ast(content)
-
-def bfs_for_ifStat_cond_ast(contents):
-    print(contents)
-
-def bfs_for_ifStat_scope_ast(contents):
-    print(contents)
-
-def bfs_for_switch_cond_ast(contents):
-    print(contents)
-
-def bfs_for_switch_ksv_ast(contents):
-    print(contents)
-
-def bfs_for_normalStat_ast(contents) :
-    if contents[0] in stmtList_no_expr:
-        print("DP : ", contents)
-    elif contents[0] in stmtList_expr_in_1st:
-        if type(contents[1]) is type(list()):
-            ext_content = copy.deepcopy(contents)
-            ext_content[1] = 'extended'
-            print("DP : ", ext_content)
-
-            bfs_for_ast(contents[1])
-        else:
-            print("DP : ", contents)
-    elif contents[0] in stmtList_expr_in_2nd:
-        if type(contents[2]) is type(list()):
-            ext_content = copy.deepcopy(contents)
-            ext_content[2] = 'extended'
-            print("DP : ", ext_content)
-
-            bfs_for_ast(contents[2])
-        else:
-            print("DP : ", contents)
-    else:
-        print("DP : ", contents)
-
 if __name__ == '__main__' :
     a, d, dx = AnalyzeAPK(targetPath)
 
     for method in dx.get_methods():
         m_ast = create_ast(method)
-        parse_ast(m_ast)
+
+        ap = ASTParser()
+
+        ap.load_ast(m_ast)
+        ap.parse_ast()
+
         break
